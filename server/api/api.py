ed9f61dec4547ddfaa67d9b27396d29cc8a07695
Gaston <gastonamengual@icloud.com>
2023-04-20 20:58:50 -0300

    Add typing

--- server/api/api.py
+++ server/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, File, UploadFile, Response
 
 import numpy as np
+from numpy.typing import ArrayLike
 from server.models.model import Model, ModelConfig
 
 router = APIRouter()
@@ -10,19 +11,14 @@ ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg"]
 
 @router.post("/detect_objects/")
 async def detect_objects(image: UploadFile = File()):
-    image_file = await image.read()
+    image_file: bytes = await image.read()
 
-    image_filename = image.filename
-    if image_filename is None or image_filename is "":
-        raise ValueError("No image Selected")
+    image_filename: str = image.filename
+    
+    validate_image_filename(image_filename)
+    validate_correct_extension(image_filename)
 
-    if (
-        not "." in image_filename
-        and image_filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-    ):
-        raise ValueError("Wrong filename format")
-
-    processed_image = np.frombuffer(image_file, dtype=np.uint8)
+    processed_image = process_image(image_file)
 
     model = Model(config=ModelConfig())
     img_bytes = model.detect_object(processed_image)
@@ -30,3 +26,19 @@ async def detect_objects(image: UploadFile = File()):
     response = Response(content=img_bytes.getvalue(), media_type="image/png")
 
     return response
+
+
+def validate_image_filename(image_filename: str):
+    if image_filename is "":
+        raise ValueError("No image Selected")
+
+def validate_correct_extension(image_filename: str):
+    if (
+        not "." in image_filename
+        and image_filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    ):
+        raise ValueError("Wrong filename format")
+    
+def process_image(image_file: bytes) -> ArrayLike:
+    processed_image = np.frombuffer(image_file, dtype=np.uint8)
+    return processed_image
