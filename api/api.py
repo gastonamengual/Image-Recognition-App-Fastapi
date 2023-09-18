20f5cde43d2762dba3499c3c3d41627798ba27a1
Gaston <gastonamengual@icloud.com>
2023-09-18 19:48:03 -0300

    update

--- api/api.py
+++ api/api.py
@@ -1,14 +1,6 @@
-from fastapi import FastAPI, File, Response, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from object_detection_model import model_files as model_files
-from object_detection_model.model import Model
-
-from api.aux import (
-    get_model_config,
-    process_image,
-    validate_correct_extension,
-    validate_image_filename,
-)
 
 app = FastAPI()
 app.add_middleware(
@@ -24,21 +16,21 @@ async def root():
     return {"message": "access the /detect_objects endpoint"}
 
 
-@app.post("/detect_objects/")
-async def detect_objects(image: UploadFile = File()):
-    image_file: bytes = await image.read()
+# @app.post("/detect_objects/")
+# async def detect_objects(image: UploadFile = File()):
+#     image_file: bytes = await image.read()
 
-    image_filename: str = image.filename
+#     image_filename: str = image.filename
 
-    validate_image_filename(image_filename)
-    validate_correct_extension(image_filename)
+#     validate_image_filename(image_filename)
+#     validate_correct_extension(image_filename)
 
-    processed_image = process_image(image_file)
+#     processed_image = process_image(image_file)
 
-    model_config = get_model_config()
-    model = Model(model_config)
-    img_bytes = model.detect_object(processed_image)
+#     model_config = get_model_config()
+#     model = Model(model_config)
+#     img_bytes = model.detect_object(processed_image)
 
-    response = Response(content=img_bytes.getvalue(), media_type="image/png")
+#     response = Response(content=img_bytes.getvalue(), media_type="image/png")
 
-    return response
+#     return response
