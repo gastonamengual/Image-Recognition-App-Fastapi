1a5f08fa6133af3636a43fb7af7f1c0293993c57
Gaston <gastonamengual@icloud.com>
2023-09-19 13:52:28 -0300

    update endpoint

--- client/script.js
+++ client/script.js
@@ -9,7 +9,7 @@ submitButton.addEventListener("click", () => {
     const formData = new FormData();
     formData.append("image", image);
 
-    fetch("http://127.0.0.1:8000/detect_objects", {
+    fetch("https://image-recognition-app-fastapi.vercel.app/detect_objects", {
         method: "POST",
         body: formData,
     })
