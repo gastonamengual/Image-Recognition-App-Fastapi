ee2333991e1b5b6e25309f1ff6141e06b84cbd0d
Gaston <gastonamengual@icloud.com>
2023-09-26 12:56:24 -0300

    update

---
+++ api/api.py
@@ -0,0 +1,49 @@
+import io
+import json
+
+import numpy as np
+from fastapi import FastAPI, File, Response, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from gradio_client import Client
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg"]
+
+
+@app.get("/")
+async def root():
+    return {"message": "access the /detect_objects endpoint"}
+
+
+@app.post("/detect_objects")
+async def detect_objects(image: UploadFile = File()) -> list[int]:
+    image_file: bytes = await image.read()
+
+    if image.filename == "":
+        raise ValueError("No image Selected")
+
+    if (
+        "." not in image.filename
+        and image.filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    ):
+        raise ValueError("Wrong filename format")
+
+    image_bytes = io.BytesIO(image_file).read()
+    list_encoded_image = np.frombuffer(image_bytes, dtype=np.uint8).tolist()
+
+    client = Client("https://gastonamengual-object-detection-app.hf.space/")
+    result = client.predict(
+        json.dumps(list_encoded_image),
+        api_name="/predict",
+    )
+
+    bytes_image = bytes(json.loads(result))
+    response = Response(content=bytes_image, media_type="image/png")
+    return response
---
+++ tests/test_api.py
@@ -0,0 +1,25 @@
+import asyncio
+import io
+import os
+
+import pytest
+from fastapi import Response, UploadFile
+
+from api.api import detect_objects
+
+
+@pytest.fixture
+def sample_img_url() -> str:
+    current_dir = os.getcwd()
+    return f"{current_dir}/tests/sample_images/computer.jpg"
+
+
+def test_detect_objects(sample_img_url: str):
+    with open(sample_img_url, "rb") as image_file:
+        image_bytes = io.BytesIO(image_file.read())
+
+    upload_file = UploadFile(filename="computer.jpg", file=image_bytes)
+
+    response = asyncio.run(detect_objects(upload_file), debug=True)
+
+    assert isinstance(response, Response)
