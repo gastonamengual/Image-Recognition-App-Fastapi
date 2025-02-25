355035226c088ffa7c085338e15d0ef08083275c
gastonamengual <gastonamengual@icloud.com>
2025-02-25 15:37:40 +0100

    Update __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from .main import detect_objects
-from .validations.validations import validate_filename
+from app.main import detect_objects
+from app.validations.validations import validate_filename
 
 __all__ = ["detect_objects", "validate_filename"]
