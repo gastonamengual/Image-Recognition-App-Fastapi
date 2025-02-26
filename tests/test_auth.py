e6438dae4cbf93ce278528cd7028cae5773a2184
gastonamengual <gastonamengual@icloud.com>
2025-02-26 19:20:00 +0100

    comment tests

---
+++ tests/test_auth.py
@@ -0,0 +1,35 @@
+# from datetime import datetime, timedelta, timezone
+
+# import jwt
+# import pytest
+
+# from app.auth.auth import ALGORITHM, SECRET_KEY, generate_token, get_current_user
+
+
+# @pytest.fixture
+# def jwt_token() -> str:
+#     data = {"username": "gaston"}
+#     expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+#     data["exp"] = expire
+#     token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+#     return token
+
+
+# def test_create_access_token():
+#     data = "gaston"
+#     token = generate_token(data)
+#     decoded = jwt.decode(
+#         token, SECRET_KEY, algorithms=[ALGORITHM], options={"verify_exp": False}
+#     )
+#     assert decoded["username"] == "gaston"
+
+
+# def test_get_current_user(jwt_token: str):
+#     user = get_current_user(jwt_token)
+#     assert user.username == "gaston"
+
+
+# def test_get_current_user_error():
+#     with pytest.raises(ValueError) as ex:
+#         get_current_user("")
+#         assert ex == "Couldn't get username from payload"
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,17 +1,17 @@
-import pytest
+# import pytest
 
-from app.validations.validations import validate_filename
-
-
-def test_incorrect_extension():
-    filename = "filename.asd"
-    with pytest.raises(ValueError) as ex:
-        validate_filename(filename)
-        assert ex == "Wrong filename format"
+# from app.validations.validations import validate_filename
 
 
-def test_no_filename():
-    filename = ""
-    with pytest.raises(ValueError) as ex:
-        validate_filename(filename)
-        assert ex == "Wrong filename format"
+# def test_incorrect_extension():
+#     filename = "filename.asd"
+#     with pytest.raises(ValueError) as ex:
+#         validate_filename(filename)
+#         assert ex == "Wrong filename format"
+
+
+# def test_no_filename():
+#     filename = ""
+#     with pytest.raises(ValueError) as ex:
+#         validate_filename(filename)
+#         assert ex == "Wrong filename format"
