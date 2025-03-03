637d46392d845e0faa9ef1d6f390a28f2db85aec
gastonamengual <gastonamengual@icloud.com>
2025-03-03 20:37:27 +0100

    Update CI pipeline to use latest pypa/gh-action-pypi-publish and add long description to setup.py

--- setup.py
+++ setup.py
@@ -22,4 +22,6 @@ setup(
     author_email="your.email@example.com",
     install_requires=[required],
     include_package_data=True,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
